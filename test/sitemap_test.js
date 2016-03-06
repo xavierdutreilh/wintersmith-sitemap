5e750de552bc5422fffa3da16182c33f1ec36d14
Xavier Dutreilh <xavier@dutreilh.com>
2016-03-06 21:19:57 +0100

    Use ES6 features in test

--- test/sitemap_test.js
+++ test/sitemap_test.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var fs = require('fs');
+const fs = require('fs');
 
 function read(filename) {
   return fs.readFileSync(filename, {'encoding': 'utf8'});
 }
 
 exports.sitemap = {
-  'build': function(test) {
-    var actual = read('tmp/sitemap.xml');
-    var expected = read('test/expected/sitemap.xml');
+  'build': (test) => {
+    const actual = read('tmp/sitemap.xml');
+    const expected = read('test/expected/sitemap.xml');
 
     test.equal(actual, expected, 'should build the sitemap');
 
