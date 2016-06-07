ebbb2e6b38fd3512d96583786d92c7c47966faa1
Xavier Dutreilh <xavier@dutreilh.com>
2016-06-07 21:34:39 +0200

    Refactor test

--- test/sitemap_test.js
+++ test/sitemap_test.js
@@ -1,15 +1,16 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 
 function read(filename) {
-  return fs.readFileSync(filename, {'encoding': 'utf8'});
+  return fs.readFileSync(filename, 'utf8');
 }
 
 exports.sitemap = {
   'build': (test) => {
-    const actual = read('tmp/sitemap.xml');
-    const expected = read('test/expected/sitemap.xml');
+    const actual = read(path.join(__dirname, '..', 'tmp', 'sitemap.xml'));
+    const expected = read(path.join(__dirname, 'expected', 'sitemap.xml'));
 
     test.equal(actual, expected, 'should build the sitemap');
 
